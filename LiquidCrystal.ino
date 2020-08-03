59f732c72864490d4573f6fc0cc69ce7a51df67f
Wenlin Huang <jackhwl@hotmail.com>
2020-08-03 09:42:33 -0400

    switch to vscode ide

--- LiquidCrystal.ino
+++ LiquidCrystal.ino
@@ -51,7 +51,6 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int resetTime = 3600;
 unsigned long previousMillis = 0;
 
-
 /* 
   Nokia Tune
   Connect a piezo buzzer or speaker to pin 8 or select a new pin.
@@ -59,98 +58,98 @@ unsigned long previousMillis = 0;
                                               
                                               Robson Couto, 2019
 */
-#define NOTE_B0  31
-#define NOTE_C1  33
+#define NOTE_B0 31
+#define NOTE_C1 33
 #define NOTE_CS1 35
-#define NOTE_D1  37
+#define NOTE_D1 37
 #define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
+#define NOTE_E1 41
+#define NOTE_F1 44
 #define NOTE_FS1 46
-#define NOTE_G1  49
+#define NOTE_G1 49
 #define NOTE_GS1 52
-#define NOTE_A1  55
+#define NOTE_A1 55
 #define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
+#define NOTE_B1 62
+#define NOTE_C2 65
 #define NOTE_CS2 69
-#define NOTE_D2  73
+#define NOTE_D2 73
 #define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
+#define NOTE_E2 82
+#define NOTE_F2 87
 #define NOTE_FS2 93
-#define NOTE_G2  98
+#define NOTE_G2 98
 #define NOTE_GS2 104
-#define NOTE_A2  110
+#define NOTE_A2 110
 #define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
+#define NOTE_B2 123
+#define NOTE_C3 131
 #define NOTE_CS3 139
-#define NOTE_D3  147
+#define NOTE_D3 147
 #define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
+#define NOTE_E3 165
+#define NOTE_F3 175
 #define NOTE_FS3 185
-#define NOTE_G3  196
+#define NOTE_G3 196
 #define NOTE_GS3 208
-#define NOTE_A3  220
+#define NOTE_A3 220
 #define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
+#define NOTE_B3 247
+#define NOTE_C4 262
 #define NOTE_CS4 277
-#define NOTE_D4  294
+#define NOTE_D4 294
 #define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
+#define NOTE_E4 330
+#define NOTE_F4 349
 #define NOTE_FS4 370
-#define NOTE_G4  392
+#define NOTE_G4 392
 #define NOTE_GS4 415
-#define NOTE_A4  440
+#define NOTE_A4 440
 #define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
+#define NOTE_B4 494
+#define NOTE_C5 523
 #define NOTE_CS5 554
-#define NOTE_D5  587
+#define NOTE_D5 587
 #define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
+#define NOTE_E5 659
+#define NOTE_F5 698
 #define NOTE_FS5 740
-#define NOTE_G5  784
+#define NOTE_G5 784
 #define NOTE_GS5 831
-#define NOTE_A5  880
+#define NOTE_A5 880
 #define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
+#define NOTE_B5 988
+#define NOTE_C6 1047
 #define NOTE_CS6 1109
-#define NOTE_D6  1175
+#define NOTE_D6 1175
 #define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
+#define NOTE_E6 1319
+#define NOTE_F6 1397
 #define NOTE_FS6 1480
-#define NOTE_G6  1568
+#define NOTE_G6 1568
 #define NOTE_GS6 1661
-#define NOTE_A6  1760
+#define NOTE_A6 1760
 #define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
+#define NOTE_B6 1976
+#define NOTE_C7 2093
 #define NOTE_CS7 2217
-#define NOTE_D7  2349
+#define NOTE_D7 2349
 #define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
+#define NOTE_E7 2637
+#define NOTE_F7 2794
 #define NOTE_FS7 2960
-#define NOTE_G7  3136
+#define NOTE_G7 3136
 #define NOTE_GS7 3322
-#define NOTE_A7  3520
+#define NOTE_A7 3520
 #define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
+#define NOTE_B7 3951
+#define NOTE_C8 4186
 #define NOTE_CS8 4435
-#define NOTE_D8  4699
+#define NOTE_D8 4699
 #define NOTE_DS8 4978
-#define REST      0
+#define REST 0
 
-#define ELEMENTSIZE(x)   (sizeof(x) / sizeof(x[0]))
+#define ELEMENTSIZE(x) (sizeof(x) / sizeof(x[0]))
 
 // change this to whichever pin you want to use
 int buzzer = 8;
@@ -162,39 +161,103 @@ int buzzer = 8;
 
 int melody_Nokia[] = {
 
-  // Nokia Ringtone 
-  // Score available at https://musescore.com/user/29944637/scores/5266155
-  // tempo 180
-  
-  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
-  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
-  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
-  NOTE_A4, 2, 
+    // Nokia Ringtone
+    // Score available at https://musescore.com/user/29944637/scores/5266155
+    // tempo 180
+
+    NOTE_E5,
+    8,
+    NOTE_D5,
+    8,
+    NOTE_FS4,
+    4,
+    NOTE_GS4,
+    4,
+    NOTE_CS5,
+    8,
+    NOTE_B4,
+    8,
+    NOTE_D4,
+    4,
+    NOTE_E4,
+    4,
+    NOTE_B4,
+    8,
+    NOTE_A4,
+    8,
+    NOTE_CS4,
+    4,
+    NOTE_E4,
+    4,
+    NOTE_A4,
+    2,
 };
 
 int melody_HappyBirthDay[] = {
 
-  // Happy Birthday
-  // Score available at https://musescore.com/user/8221/scores/26906
-  // tempo 140
+    // Happy Birthday
+    // Score available at https://musescore.com/user/8221/scores/26906
+    // tempo 140
 
-  NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
-  NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
-  NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
+    NOTE_C4,
+    4,
+    NOTE_C4,
+    8,
+    NOTE_D4,
+    -4,
+    NOTE_C4,
+    -4,
+    NOTE_F4,
+    -4,
+    NOTE_E4,
+    -2,
+    NOTE_C4,
+    4,
+    NOTE_C4,
+    8,
+    NOTE_D4,
+    -4,
+    NOTE_C4,
+    -4,
+    NOTE_G4,
+    -4,
+    NOTE_F4,
+    -2,
+    NOTE_C4,
+    4,
+    NOTE_C4,
+    8,
 
-  NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4, 
-  NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
-  NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
-  NOTE_F4,-2,
- 
+    NOTE_C5,
+    -4,
+    NOTE_A4,
+    -4,
+    NOTE_F4,
+    -4,
+    NOTE_E4,
+    -4,
+    NOTE_D4,
+    -4,
+    NOTE_AS4,
+    4,
+    NOTE_AS4,
+    8,
+    NOTE_A4,
+    -4,
+    NOTE_F4,
+    -4,
+    NOTE_G4,
+    -4,
+    NOTE_F4,
+    -2,
+
 };
 
 int divider = 0, noteDuration = 0;
-char buffer [8];
+char buffer[8];
 
-void playTheTone(int melody[], int sizeOfMelody, int tempo) {
+void playTheTone(int melody[], int sizeOfMelody, int tempo)
+{
   // change this to make the song slower or faster
   //int tempo = 180;
 
@@ -208,14 +271,18 @@ void playTheTone(int melody[], int sizeOfMelody, int tempo) {
 
   // iterate over the notes of the melody.
   // Remember, the array is twice the number of notes (notes + durations)
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+  {
 
     // calculates the duration of each note
     divider = melody[thisNote + 1];
-    if (divider > 0) {
+    if (divider > 0)
+    {
       // regular note, just proceed
       noteDuration = (wholenote) / divider;
-    } else if (divider < 0) {
+    }
+    else if (divider < 0)
+    {
       // dotted notes are represented with negative durations!!
       noteDuration = (wholenote) / abs(divider);
       noteDuration *= 1.5; // increases the duration in half for dotted notes
@@ -232,14 +299,17 @@ void playTheTone(int melody[], int sizeOfMelody, int tempo) {
   }
 }
 
-void clearLCDLine(int line){
- for(int n = 0; n < 16; n++) {  // 20 indicates symbols in line. For 2x16 LCD write - 16
-   lcd.setCursor(n,line);
-   lcd.print(" ");
- }
+void clearLCDLine(int line)
+{
+  for (int n = 0; n < 16; n++)
+  { // 20 indicates symbols in line. For 2x16 LCD write - 16
+    lcd.setCursor(n, line);
+    lcd.print(" ");
+  }
 }
 
-void setup() {
+void setup()
+{
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
@@ -248,31 +318,34 @@ void setup() {
   pinMode(buzzer, OUTPUT);
 }
 
-void setTimeBuffer(char* buffer, unsigned long seconds, bool withHour){
+void setTimeBuffer(char *buffer, unsigned long seconds, bool withHour)
+{
   uint8_t hours, minutes;
   minutes = seconds / 60;
   hours = minutes / 60;
   if (withHour)
-    sprintf (buffer, "%02u:%02u:%02u", hours, minutes, seconds%60);
-  else 
-    sprintf (buffer, "%02u:%02u", minutes, seconds%60);
+    sprintf(buffer, "%02u:%02u:%02u", hours, minutes % 60, seconds % 60);
+  else
+    sprintf(buffer, "%02u:%02u", minutes, seconds % 60);
 }
 
-void loop() {
+void loop()
+{
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   unsigned long currentMillis = millis();
   // print the number of seconds since rollover:
-  setTimeBuffer(buffer, (currentMillis - previousMillis)/1000, false);
+  setTimeBuffer(buffer, (currentMillis - previousMillis) / 1000, false);
   lcd.write(buffer);
   //lcd.print((currentMillis - previousMillis)/1000);
   lcd.setCursor(8, 1);
 
-  setTimeBuffer(buffer, currentMillis/1000, true);
+  setTimeBuffer(buffer, currentMillis / 1000, true);
   lcd.write(buffer);
-  
-  if ((unsigned long)(currentMillis - previousMillis)/1000 >= resetTime) {
+
+  if ((unsigned long)(currentMillis - previousMillis) / 1000 >= resetTime)
+  {
     previousMillis = currentMillis;
     lcd.setCursor(0, 1);
     lcd.print("It's Tea Time!");
