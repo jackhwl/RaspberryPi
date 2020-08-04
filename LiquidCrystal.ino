b549d9b2b372d817d2b6df71427763dd076230eb
Wenlin Huang <jackhwl@hotmail.com>
2020-08-04 11:09:04 -0400

    replace hours with int type

--- LiquidCrystal.ino
+++ LiquidCrystal.ino
@@ -63,7 +63,7 @@ void setup()
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Hello viGlobal!");
+  //lcd.print("Hello viGlobal!");
 
   pinMode(buzzer, OUTPUT);
 }
